0ffd7842329349a361547333e02a4d231771a683
211217613 <rubenavazquez@gmail.com>
2016-03-30 16:28:34 -0700

    Deleting old stuff

--- Sesion5/E5_1.cpp
+++ Sesion5/E5_1.cpp
@@ -1,31 +1,46 @@
-int main(){
-	
-	// Declare  Imagen of type Mat: Mat is a CV object where images are stored
+
+/*
+g++ -Wall -o salida E5_2.cpp `pkg-config --cflags --libs opencv`
+
+*/
+
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream> 
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+using namespace std;
+
+using namespace cv;
+
+
+
+int main(void){
 	Mat Imagen;
-	
-	// Create an object cap(0) of type VideoCapture
+
 	VideoCapture cap(0);
 	
-	if( !cap.isOpened() ){
-		return -1;
-	}
+	if(!cap.isOpened())	return -1;
 	
 	while(1){
+
 		cap >> Imagen;
-		
-		cvtColor(Imagen, Imagen, CV_RGB2HSV);
-		
-		GaussianBlur(Imagen, Imagen, Size(7, 7), 0, 0);
-		
-		// es el rango a trabajar
-		inRange(Imagen, Scalar(50,50,50), Scalar(90,90,90), Imagen);
-		
-		// display Imagen and title window Camera1
-		imshow("Camera1", Imagen)
-		
-		if( waitKey(15) > 0 ){
-			break;
-		}
+
+		cvtColor(Imagen,Imagen,CV_RGB2HSV);
+
+		GaussianBlur(Imagen, Imagen, Size(7, 7),0,0);
+
+		inRange(Imagen, Scalar(50,50,50), Scalar(90,90,90) ,Imagen);
+		imshow("Camara 1", Imagen);
+		if(waitKey(30)>0) break;
 	}
-	
 }
